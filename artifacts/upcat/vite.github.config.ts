80abcf28d706d741fb74ed6fc77e9c3d52d2595a
jjaden <jadenjoshalba@gmail.com>
2026-07-12 23:10:11 +0800

    Fix

--- artifacts/upcat/vite.github.config.ts
+++ artifacts/upcat/vite.github.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/jcetrev/",
+  base: "/KolehiyoTrack/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
